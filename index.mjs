4d01c605a86166be3fb5cc228e045087a856cfed
Federico Malfasi <federico.malfasi@gmail.com>
2025-08-04 21:50:44 -0300

    Calculator

---
+++ index.mjs
@@ -0,0 +1,23 @@
+//
+// Calculadora
+
+/*
+Tiene que tener
+
+- Clases Calculadora
+- Metodos Sumar, Restar, Multiplicar, Dividir, Resto, Potencia
+- Menú Interactivo
+- Tiene que estar dividido en archivos
+
+*/
+//
+// Calculadora
+
+/*
+Tiene que tener
+
+- Clases Calculadora
+- Metodos Sumar, Restar, Multiplicar, Dividir, Resto, Potencia
+- Menú Interactivo
+- Tiene que estar dividido en archivos
+*/
